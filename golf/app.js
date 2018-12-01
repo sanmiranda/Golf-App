cc703f62a6b7f0a9e34a2bd817e4a8cc7acb1ea0
sanmiranda <sanmiranda@hotmail.com>
2018-11-30 19:06:07 -0600

    va2

--- golf/app.js
+++ golf/app.js
@@ -53,6 +53,6 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
-
-
+// const profile = require('./routes/profile')
+// app.use('/profile', profile)
 module.exports = app;
